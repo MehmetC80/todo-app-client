a4243ad7737031d3eb7af8428a3dc469b2214a6b
Mehmet Cetinkaya <m.cetinkaya@hotmail.de>
2023-01-29 12:45:46 +0100

    add todo interface and json fake db

---
+++ src/db/mock-todos.ts
@@ -0,0 +1,34 @@
+import { Todo } from 'src/app/interfaces/todo';
+
+export const TODOS: Todo[] = [
+  {
+    id: 1,
+    text: 'JavaScript lernen',
+    day: 'Dezember 2022 um 13:40',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'TypeScript lernen',
+    day: 'November 2022 um 14:30',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'Angular lernen',
+    day: ' Januar 2023 um 16:40',
+    reminder: true,
+  },
+  {
+    id: 4,
+    text: 'Java lernen',
+    day: 'Februar 2023 um 12:40',
+    reminder: true,
+  },
+  {
+    id: 1,
+    text: 'Spring Framework lernen',
+    day: 'März 2023 um 13:40',
+    reminder: true,
+  },
+];
